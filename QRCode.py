cbaf45600bd86d30ebceafd09d59ebe3fd9ac0f0
panchalpriya22062007 <128357600+panchalpriya22062007@users.noreply.github.com>
2025-05-20 21:54:46 +0530

    QRCode.py

--- QRCode.py
+++ QRCode.py
@@ -3,5 +3,18 @@ import qrcode
 img = qrcode.make("https://www.openai.com")
 
 img.save("openai_qr.png")
+# to customize qrcode
 
+import qrcode as qr
+from PIL import Image
+qr=qr.QRCode(
+version=1, 
+error_correction=qr.constants.ERROR_CORRECT_H,
+box_size=10,
+border=5)
+qr.add_data("https://www.openai.com")
+qr.make(fit=True)
+img=qr.make_image(fill_color="purple",back_color="white")
+img.show()
+image.save("openai_qr.png")
 
